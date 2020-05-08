7f8178663fd2edad78c929390dc6fd41c1fb7d5d
Sarthak Sarna <sarthaksarna9625@gmail.com>
2020-05-08 03:28:15 -0400

    First commit

---
+++ src/build/precache-manifest.27c2f9bd45c0e31486bf03249591aef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8039d56f15bb9ac8ed364d8073e03b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a7bbb4ca4c84503367c",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "7620550cfd244b45208f",
+    "url": "/static/js/2.5dfc5463.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5dfc5463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7bbb4ca4c84503367c",
+    "url": "/static/js/main.530bfc65.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
