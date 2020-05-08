fe207d7e21bbc5f8e00d612ffb200583dd69b4d9
Sarthak Sarna <sarthaksarna@users.noreply.github.com>
2020-05-08 06:07:22 -0400

    Add files via upload

--- src/build/service-worker.js
+++ src/build/service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27c2f9bd45c0e31486bf03249591aef7.js"
+  "/precache-manifest.7d9a413c38db1b17d8201cfa0d06e33c.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/server.js
+++ src/server.js
@@ -3,6 +3,22 @@ import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 import path from 'path';
 
+[
+    {
+        name: 'learn-react',
+        upvotes: 0,
+        comments: [],
+    }, {
+        name: 'learn-node',
+        upvotes: 0,
+        comments: [],
+    }, {
+        name: 'my-thoughts-on-resumes',
+        upvotes: 0,
+        comments: [],
+    },
+]
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/build')));
